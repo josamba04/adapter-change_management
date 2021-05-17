5a8355ce5cd24871cc7dfc34c60bacf1236e8f4c
josamba04 <julius.osamba@verizonwireless.com>
2021-05-17 15:43:48 +0000

    Lab 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Update this constant with your ServiceNow credentials
 const options = {
-  url: 'https://developer.servicenow.com/dev.do#!/',
+  url: 'https://developer.servicenow.com/dev.do',
   username: 'admin',
   password: 'C90mwlueRHZGJ'
 };
